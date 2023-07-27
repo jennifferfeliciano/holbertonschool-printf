12753cfef453612df893f597f80dbcd85bc67f26
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-07-27 18:17:41 +0000

    prueba incompleta

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
-#include "utility_functions.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 /**
  *
@@ -9,9 +11,36 @@
 
 int _printf(const char *format, ...)
 {
+	int counter = 0;
+	char data_type;
 
 
+	va_list arguments;
+	
+	va_start(arguments, format);
+	
+	if (format == NULL)
+		return (-1);
+	else
+		for (; format[counter] != '\0'; counter++)
+		{
+			if (format[counter] == '%')
+	
+				data_type = (format[counter + 1]);	
+			
 
+					if (data_type == 'c') 
 
+						print_char(va_arg(arguments, int));
 
+					
+					else if (data_type == 's')
+
+						print_string(va_arg(arguments, char *));
+					else if (data_type == 'i')
+						print_int(va_arg(arguments, int));
+		}
+
+	va_end(arguments);
+	return (counter);
 }
--- utility_functions.c
+++ utility_functions.c
@@ -1,24 +1,40 @@
+
 #include "main.h"
+#include <stddef.h>
+#include <stdarg.h>
 
 /**
- *
- *
- *
- *
+ *print_char- function that prints a char
+ *@c: character to be printed
+ *Return: 
  */
 
-int print_char(char c);
+int print_char(char c)
 {
+	if (c == '\0')
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+	}
+
+	else
+		_putchar(c);
+
+	return (1);
 }
 
 
 /**
  *print_string- function that prints a string
- *@c: pointer to the first character of the string
+ *@s: pointer to the first character of the string
  *Return: length of the string
  */
 
-int print_string(char *s);
+int print_string(char *s)
 {
 	int counter = 0;
 
@@ -34,24 +50,10 @@ int print_string(char *s);
 
 	else
 	{
-		for(; s[counter] != '\0'; counter++)
+		for(; *s != '\0'; counter++)
 
 			_putchar(s[counter]);
 	}
 
 	return (counter);
-}
-
-
-
-
-
-/**
- *
- *
- *
- */
-
-int print_int(int n);
-{
 }
