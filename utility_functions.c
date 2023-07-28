6a514a293f78de2b35aac3cc987cba7a6d1967ef
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-07-28 03:18:05 +0000

    Create helper functions for integers, decimals, characters and strings

--- utility_functions.c
+++ utility_functions.c
@@ -2,11 +2,11 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdarg.h>
-
+#include <stdio.h>
 /**
  *print_char- function that prints a char
  *@c: character to be printed
- *Return: 
+ *Return: 1 if non-null character printed, else 0 for null character.
  */
 
 int print_char(char c)
@@ -19,6 +19,8 @@ int print_char(char c)
 		_putchar('l');
 		_putchar('l');
 		_putchar(')');
+
+	return (0);
 	}
 
 	else
@@ -40,20 +42,48 @@ int print_string(char *s)
 
 	if (s == NULL)
 	{
-		_putchar('(');
-		_putchar('n');
-		_putchar('u');
-		_putchar('l');
-		_putchar('l');
-		_putchar(')');
+		return (0);
+	}
+
+	for ( ; s[counter] != '\0'; counter++)
+	{
+		_putchar(s[counter]);
+	}
+
+	return (counter);
+}
+
+/**
+ *print_int- function that prints an integer
+ *@n: number to be printed
+ *Return: number of digits of n
+ */
+
+int print_int(int n)
+{
+	int counter = 0;
+	unsigned int number;
+
+	if (n < 0)
+	{
+
+		_putchar('-');
+		counter++;
+		number = -n;
 	}
 
 	else
 	{
-		for(; *s != '\0'; counter++)
-
-			_putchar(s[counter]);
+		number = n;
 	}
+
+
+
+	if (number / 10)
+	{
+		counter += print_int(number / 10);
+	}
+	_putchar((number % 10) + '0');
 
 	return (counter);
 }
