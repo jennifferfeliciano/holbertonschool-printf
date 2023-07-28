c3523a8c5f51ef7fd9863a4cf5c07252adc7b0bb
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-07-28 03:10:36 +0000

    Create custum function that emulates printf. add if statement that checks for differents especifiers.

--- _printf.c
+++ _printf.c
@@ -1,46 +1,51 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stddef.h>
 
 /**
- *
- *
- *
+ *_printf- Custum function that emulates the funtion printf
+ *@format: String specifying the print layout
+ *Return: number of characters printed or -1 if error
  */
 
 int _printf(const char *format, ...)
 {
 	int counter = 0;
 	char data_type;
-
-
 	va_list arguments;
-	
+
 	va_start(arguments, format);
-	
-	if (format == NULL)
+
+	if (format == NULL || arguments == NULL)
+	{
 		return (-1);
-	else
-		for (; format[counter] != '\0'; counter++)
+	}
+
+	for (; format[counter] != '\0'; counter++)
+	{
+		if (format[counter] == '%')
 		{
-			if (format[counter] == '%')
-	
-				data_type = (format[counter + 1]);	
-			
-
-					if (data_type == 'c') 
-
-						print_char(va_arg(arguments, int));
-
-					
-					else if (data_type == 's')
-
-						print_string(va_arg(arguments, char *));
-					else if (data_type == 'i')
-						print_int(va_arg(arguments, int));
+			data_type = (format[counter + 1]);
+			if (data_type == 'c')
+				print_char(va_arg(arguments, int));
+			else if (data_type == 's')
+				print_string(va_arg(arguments, char *));
+			else if (data_type == 'i' || data_type == 'd')
+				print_int(va_arg(arguments, int));
+			else if (data_type == '%')
+				_putchar('%');
+			else
+			{
+				_putchar('%');
+				_putchar(format[counter + 1]);
+			}
+			counter++;
 		}
-
+		else
+		{
+			_putchar(format[counter]);
+		}
+	}
 	va_end(arguments);
 	return (counter);
 }
